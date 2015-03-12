388492e126163030c0d8e1d5fce7efc2b72d8830
Rob Taylor <robert.i.taylor88@gmail.com>
2015-03-12 21:44:01 +0000

    implemented basic keys. Still need to protect record key value (set to final?)

--- database/DB.java
+++ database/DB.java
@@ -8,55 +8,56 @@ class DB{
 
 	public void run(){
 
-		// Table testTable = new Table("Dogs", 3);
-		// testTable.addColumn("Name", 1);
-		// testTable.addColumn("Age", 2);
-		// testTable.addColumn("Weight(pounds)", 3);
+		Table testTable = new Table("Dogs", 3);
+		testTable.addColumn("Name", 1);
+		testTable.addColumn("Age", 2);
+		testTable.addColumn("Weight(pounds)", 3);
 
-		// Record testRecord = new Record(5);
+		Record testRecord = new Record(5, "Gromit");
 		// testRecord.addField("Gromit ", 1);
-		// String testRecordField = testRecord.getField(1).getName();
+		String testRecordField = testRecord.getField(1).getName();
 		// System.out.println(testRecordField);
 
-		// testRecord.addField("3 ", 2);
-		// testRecordField = testRecord.getField(2).getName();
-		// System.out.println(testRecordField);
+		testRecord.addField("3 ", 2);
+		testRecordField = testRecord.getField(2).getName();
+		System.out.println(testRecordField);
 
-		// testRecord.addField("45 ", 3);
-		// testRecordField = testRecord.getField(3).getName();
-		// System.out.println(testRecordField);
+		testRecord.addField("45 ", 3);
+		testRecordField = testRecord.getField(3).getName();
+		System.out.println(testRecordField);
 
-		// Record testRecord1 = new Record(5);
+		Record testRecord1 = new Record(5, "Nobber");
+		Record testRecord2 = new Record(5, "Nobber");
 		// testRecord1.addField("Nobber ", 1);
 		// String testRecord1Field = testRecord1.getField(1).getName();
 		// System.out.println(testRecord1Field);
 
-		// testRecord1.addField("5 ", 2);
-		// testRecord1Field = testRecord1.getField(2).getName();
-		// System.out.println(testRecord1Field);
+		testRecord1.addField("5 ", 2);
+		String testRecord1Field = testRecord1.getField(2).getName();
+		System.out.println(testRecord1Field);
 
-		// testRecord1.addField("150 ", 3);
-		// testRecord1Field = testRecord1.getField(3).getName();
-		// System.out.println(testRecord1Field);
+		testRecord1.addField("150 ", 3);
+		testRecord1Field = testRecord1.getField(3).getName();
+		System.out.println(testRecord1Field);
 
-		// testTable.addRecord(testRecord);
-		// testTable.addRecord(testRecord1);
+		testTable.addRecord(testRecord);
+		testTable.addRecord(testRecord1);
 
-		// testTable.printTable();
+		testTable.printTable();
 
 		// testTable.save("tables");
 
-		Database testDatabase = new Database();
+		// Database testDatabase = new Database();
 
-		testDatabase.loadDatabase("tables");
-		Table testTable = null; 
-		if(testDatabase.getTable("Dogs ") != null){
-			testTable = testDatabase.getTable("Dogs ");
-			testTable.printTable();
-		}
-		else{
-			System.out.println("null table");
-		}
+		// testDatabase.loadDatabase("tables");
+		// Table testTable = null; 
+		// if(testDatabase.getTable("Dogs ") != null){
+		// 	testTable = testDatabase.getTable("Dogs ");
+		// 	testTable.printTable();
+		// }
+		// else{
+		// 	System.out.println("null table");
+		// }
 
 
 		// testDatabase.saveDatabase();
--- database/Record.java
+++ database/Record.java
@@ -2,13 +2,34 @@ import java.util.ArrayList;
 
 class Record{
 
+	private String name;
+	static ArrayList<String> recordKeysArray = new ArrayList<String>();
 	private int fieldCount = 0;
 	private ArrayList<Field> fieldsArray = new ArrayList<Field>();
 
-	//Record constructor. Number of fields must be specified
-	public Record(int fieldCount){
+	//Record constructor. Number of fields must be specified, as well as first field
+	//First field acts as key. Uniqueness is checked;
+	public Record(int fieldCount, String keyField){
+		if(keyExists(keyField, recordKeysArray)){
+			System.out.println("Key exists. Try another name for Record " + keyField);
+			System.exit(1);
+		}
+		else{
+		this.name = keyField;
+		recordKeysArray.add(keyField);
+		addField(keyField, 1);
 		this.fieldCount = fieldCount;
 		fieldsArray.ensureCapacity(fieldCount);
+		}
+	}
+
+	private boolean keyExists(String nameToCheck, ArrayList<String> recordNamesArray){
+		for(int i=0; i<recordNamesArray.size(); i++){
+			if(recordNamesArray.get(i).equals(nameToCheck)){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	//adds new field to specific column in Record
